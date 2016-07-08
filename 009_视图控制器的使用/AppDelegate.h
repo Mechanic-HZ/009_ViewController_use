fcef8704634a844d06142b65c5186f924a1e61ba
古月官儿 <631042441@qq.com>
2016-07-08 10:50:42 +0800

    Initial Commit

---
+++ 009_视图控制器的使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  009_视图控制器的使用
+//
+//  Created by GeekHu on 16/7/8.
+//  Copyright © 2016年 GeekHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 009_视图控制器的使用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  009_视图控制器的使用
+//
+//  Created by GeekHu on 16/7/8.
+//  Copyright © 2016年 GeekHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
